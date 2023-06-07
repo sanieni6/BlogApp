2a0b27dca82915ae97c5fbb6a533a686a54c179f
sanieni6 <luissanchezzamora23@gmail.com>
2023-06-07 16:31:11 -0500

    Create Entries

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+fourth_user = User.create(name: 'Jaime', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+third_user = User.create(name: 'Jonh', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+
+first_post = Post.create(author_id: second_user.id, title: 'Hello', text: 'This is my first post')
+second_post = Post.create(author_id: second_user.id, title: 'Hello', text: 'This is my second post')
+third_post = Post.create(author_id: first_user.id, title: 'Hello', text: 'This is my third post')
+fourth_post = Post.create(author_id: first_user.id, title: 'Hello', text: 'This is my fourth post')
+
+Comment.create(post_id: first_post.id, author_id: second_user.id, text: 'Hi Tom!' )
+Comment.create(post_id: first_post.id, author_id: second_user.id, text: 'Hi Luis!' )
+Comment.create(post_id: second_post.id, author_id: first_user.id, text: 'Hi Ana!' )
+Comment.create(post_id: second_post.id, author_id: first_user.id, text: 'Hi Gio!' )
+
+Like.create(post_id: first_post.id, author_id: first_user.id)
+Like.create(post_id: first_post.id, author_id: first_user.id)
+Like.create(post_id: second_post.id, author_id: second_user.id)
+Like.create(post_id: second_post.id, author_id: second_user.id)
