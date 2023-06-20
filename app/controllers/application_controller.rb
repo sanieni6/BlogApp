5bfd3177edb36ff636de17846a5fc145d83bb01c
Osvaldo Barrios Cortina <osvaldo.barrios@gmail.com>
2023-06-20 17:22:19 -0500

    Fix the application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-endprotect_from_forgery with: :exception
+protect_from_forgery with: :exception
 
   before_action :update_allowed_parameters, if: :devise_controller?
 
