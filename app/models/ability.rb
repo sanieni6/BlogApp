039d9df046be36762ab2898eb3fe3948c0699fe7
sanieni6 <luissanchezzamora23@gmail.com>
2023-06-22 12:05:12 -0500

    Add can can can to the project

---
+++ app/models/ability.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    can :read, Post
+
+    return unless user.present?
+
+    can :manage, Post, author_id: user.id
+    can :manage, Comment, author_id: user.id
+
+    return unless user.role == 'admin'
+
+    can :manage, :all
+  end
+end
