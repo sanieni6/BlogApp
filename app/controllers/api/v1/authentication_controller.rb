9f50c6168b910ec4d17e691a500f2c868ae19737
Osvaldo Barrios Cortina <osvaldo.barrios@gmail.com>
2023-06-22 15:21:34 -0500

    Add comments controller for API

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,2 +1,13 @@
 class Api::V1::AuthenticationController < ApplicationController
+    
+    skip_before_action :authenticate_token!
+
+  def create
+    user = User.find_by(email: params[:user][:email])
+    if user.valid_password? params[:user][:password]
+      render json: { token: JsonWebToken.encode(sub: user.id) }
+    else
+      render json: { errors: ['Invalid email or password'] }
+    end
+  end
 end
