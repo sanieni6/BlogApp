8964d3fa97207df02a56b17393477e7506e2b9ff
sanieni6 <luissanchezzamora23@gmail.com>
2023-06-23 11:37:23 -0500

    Add api routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
     end
   end
   
-  namespace :api do
+  namespace :api, defaults: {format: 'json'} do
     namespace :v1 do
       post :auth, to: 'authentication#create'
       resources :users, only: [] do
